84553cfbd617c4b850d2c712df7c6d13dbfbd1f7
Meghan Tashi <85388258+MeghanTashi@users.noreply.github.com>
2022-06-09 17:39:06 +0530

    Add files via upload

---
+++ wrap.java
@@ -0,0 +1,11 @@
+public class wrap {
+    public static void main(String[] args) {
+        int x = 10;
+        Integer in = 100;
+        String str = "5000";
+        System.out.println(x);
+        System.out.println(in);
+        System.out.println(Integer.parseInt(str));
+        System.out.println();
+    }
+}
